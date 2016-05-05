a5064b43ac9b0df84078cfcc168e5cd7ea335778
Drew Freyling <drew.freyling@gmail.lcom>
2016-05-05 21:28:04 +1000

    refactored controller

--- src/SbManager/Scripts/manager/homeController.js
+++ src/SbManager/Scripts/manager/homeController.js
@@ -1,26 +1,40 @@
-﻿$app.controller('homeController', ['$scope', '$routeParams', function ($scope, $routeParams) {
-    $scope.refresh = function () {
-        $scope.model = null;
-        $.ajax({
-            url: window.applicationBasePath + "/api/v1/busmanager/",
-            dataType: 'json',
-            success: function(d) {
-                $scope.model = d;
-                $scope.$digest();
-            },
-            error: function (jqXHR) {
-                var err = $.parseJSON(jqXHR.responseText);
-                alert("ERROR: " + err.Title + err.Summary);
-            }
-        });
-    };
-    $scope.refresh();
+﻿(function (angular) {
+    "use strict";
 
-    $scope.deleteAll = function () {
-        if (!window.confirm("You sure? This can't be undone and your world might explode.")) return;
-        $scope.model = null;
-        $.post(window.applicationBasePath + "/api/v1/busmanager/deleteall", function (d) {
-            window.location = "#/";
-        });
-    };
-}]);
+    angular
+        .module("manager")
+        .controller ("homeController", HomeController);
+
+    HomeController.$inject = ['$scope', '$http'];
+
+    function HomeController($scope, $http) {
+        $scope.refresh = refresh;
+        $scope.deleteAll = deleteAll;
+        activate();
+
+        function activate() {
+            $scope.refresh();
+        }
+
+        function refresh() {
+            $scope.model = null;
+            return $http.get(window.applicationBasePath + '/api/v1/busmanager/')
+                .then(function(data) {
+                    $scope.model = data;
+                    $scope.$digest();
+                })
+                .catch(function(err) {
+                    alert("ERROR: " + err.message);
+                });
+        }
+
+        function deleteAll() {
+            if (!window.confirm("You sure? This can't be undone and your world might explode.")) return;
+            $scope.model = null;
+            $http.post(window.applicationBasePath + "/api/v1/busmanager/deleteall", function(d) {
+                window.location = "#/";
+            });
+        }
+
+    }
+}(angular));
